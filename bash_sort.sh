27930bd2742465671cbe79ba7c0c5c8a48424ed7
Mansur Muaz Ekici <mmuazekici@gmail.com>
2019-05-05 15:44:43 +0300

    first commit

---
+++ bash_sort.sh
@@ -0,0 +1,91 @@
+#!/bin/bash
+
+# awk -v n=100 -v seed="$RANDOM" 'BEGIN { srand(seed); for (i=0; i<n; ++i) printf("%.2f\n", rand()*1000) }' > random.txt
+
+
+insertionSort() {
+    echo "insertionSort"
+
+    arr=("$@")
+    n=${#arr[@]}
+
+    # logic for insertion sort   $(echo "arr[j] > temp" |bc -l)
+
+    for((i=1;i<n;i++)) ; do
+        j=$i-1
+        temp=${arr[$i]}
+        while(( $j>=0 && $(echo "${arr[$j]} > $temp" |bc -l) )) ; do  #arr[j]>temp
+            arr[$j+1]=${arr[$j]}
+            j=$((j-1))
+        done
+        arr[j+1]=$temp
+    done
+
+
+
+
+    echo "done"
+
+    for i in "${arr[@]}"; do
+        echo "x $i"
+    done
+
+}
+
+mergeSort() {
+    echo "mergeSort"
+}
+
+heapSort() {
+    echo "heapSort"
+}
+
+quickSort() {
+    echo "quickSort"
+}
+
+countingSort() {
+    echo "countingSort"
+}
+
+bucketSort() {
+    echo "bucketSort"
+}
+
+
+fun_read_array() {
+    arr=("$@")
+    for i in "${arr[@]}" ; do
+        echo $i
+    done
+}
+
+if [[ "$#" -ne 4 ]]; then
+    if [[ ( "$#" -eq 1 )  && ( ( $1 = "-h" ) || ( $1 = "--help") ) ]] ; then
+        echo "Help"
+    else
+        echo "Usage statement"
+    fi
+else
+    # Check $1 is -f or --filename and $2 is exist and readable file.
+    if [[ (( $1 = "-f" ) || ( $1 = "--filename")) && (-n $2 && -r $2) ]] ; then
+
+        # Check 3 is -a or --algorithm and $4 is integer between 1 - 6
+        if [[ (( $3 = "-a" ) || ( $3 = "--algorithm")) && ( "$4" =~ ^[1-6]$ ) ]] ; then
+
+            # Add inputs to array
+            while IFS='' read -r line || [[ -n "$line" ]]; do
+                numbersArray+=( "$line" )
+            done < "$2"
+
+            case $4 in
+                1) insertionSort "${numbersArray[@]}" ;;
+                2) mergeSort "${numbersArray[@]}" ;;
+                3) heapSort "${numbersArray[@]}" ;;
+                4) quickSort "${numbersArray[@]}" ;;
+                5) countingSort "${numbersArray[@]}" ;;
+                6) bucketSort "${numbersArray[@]}" ;;
+            esac
+        fi
+    fi
+fi
