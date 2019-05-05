296b2a7715f2ac66f6670795afbfa25949212d36
Mansur Muaz Ekici <mmuazekici@gmail.com>
2019-05-05 16:31:19 +0300

    Merge

--- bash_sort.sh
+++ bash_sort.sh
@@ -34,32 +34,34 @@ insertionSort() {
 }
 
 merge() {
-    local first=2
-    local second=$(( $1 + 2 ))
+	local first=2
+	local second=$(( $1 + 2 ))
 	for i in ${@:2} ; do
-    	if [[ $first -eq $(( $1 + 2 )) ]] ;	then
-    		echo ${@:$second:1} ; ((second += 1))
-    	else
-    		if [[ $second -eq $(( ${#@} + 1 )) ]] ; then
-    				echo ${@:$first:1} ; ((first += 1))
-    		else
-    			if [[ ${@:$first:1} -lt ${@:$second:1} ]] ; then
-    				echo ${@:$first:1} ; ((first += 1))
-    			else
-    				echo ${@:$second:1} ; ((second += 1))
-    			fi
-    		fi
+		if [[ $first -eq $(( $1 + 2 )) ]] ; then
+			echo ${@:$second:1} ; ((second += 1))
+		else
+			if [[ $second -eq $(( ${#@} + 1 )) ]] ; then
+				echo ${@:$first:1} ; ((first += 1))
+			else
+				if (( $(echo "${@:$first:1} < ${@:$second:1}" |bc -l) )); then
+					echo ${@:$first:1} ; ((first += 1))
+				else
+					echo ${@:$second:1} ; ((second += 1))
+				fi
+			fi
 		fi
 	done
 }
 
 mergeSort() {
-    echo "mergeSort"
-
-    local med=$(( $1 / 2 ))
-	local first=( $(mergeSort $med ${@:2:$med}) )
-	local second=( $(mergeSort $(( $1 - $med )) ${@:$(( $med + 2 )):$(( $1 - $med ))}) )
-    echo $(merge $med ${first[@]} ${second[@]})
+	if [[ $1 -ge 2 ]] ; then
+		local med=$(( $1 / 2 ))
+		local first=( $(mergeSort $med ${@:2:$med}) )
+		local second=( $(mergeSort $(( $1 - $med )) ${@:$(( $med + 2 )):$(( $1 - $med ))}) )
+		echo $(merge $med ${first[@]} ${second[@]})
+	else
+		echo $2
+	fi
 }
 
 heapSort() {
